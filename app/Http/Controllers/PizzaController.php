8785643c4d63a783b203acbad9a63c287a637d7b
francesco <francescoavitto88@gmail.com>
2024-01-13 20:46:23 +0100

    proj backend pizza_tracker inertia e laravel

---
+++ app/Http/Controllers/PizzaController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use App\Models\Pizza;
+use Illuminate\Http\Request;
+use Inertia\Response;
+
+class PizzaController extends Controller
+{
+    public function index() :Response{
+        $pizzas = Pizza::all();
+        return Inertia::render('Pizzas/All',['pizzas' => $pizzas]);
+    }
+
+    public function edit(Pizza $pizza) : Response {
+        return Inertia::render('Pizzas/Edit', ['pizza' => $pizza]);
+    }
+
+    public function update(Pizza $pizza, Request $request){
+        $pizza->update([
+            'status'=>$request->status,
+        ]);
+
+
+    }
+
+}
+
+
